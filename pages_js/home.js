75e6f4912281d66ec089f7145ea4bef98807cf0f
Samir <samirmh15@gmail.com>
2024-08-31 17:34:44 -0500

    Se corrigieron errores en buscador por texto y checkbox

--- pages_js/home.js
+++ pages_js/home.js
@@ -1,24 +1,19 @@
-import { crearTarjetas, getUniqueCategories, createCheckboxes, applyFilters} from "../modules/functions.js";
+import { crearTarjetas, getUniqueCategories, createCheckboxes, applyFilters, } from "../modules/functions.js";
 
 let url = "https://aulamindhub.github.io/amazing-api/events.json";
-let allEvents = [];  
+let allEvents = [];
 let searchInput = document.getElementById("search-input");
-let checkboxContainer = document.getElementById("checkbox-container");
-
 
 fetch(url)
     .then(res => res.json())
     .then(data => {
-       
+
         allEvents = data.events;
 
         let categoriasUnicas = getUniqueCategories(allEvents);
-
         createCheckboxes(categoriasUnicas);
-
-        
         crearTarjetas(allEvents);
-      
+
         document.querySelectorAll('.form-check-input').forEach(checkbox => {
             checkbox.addEventListener('change', () => applyFilters(allEvents, searchInput));
         });
--- pages_js/upcoming_events.js
+++ pages_js/upcoming_events.js
@@ -1,22 +1,22 @@
-import { crearTarjetas, getUniqueCategories, createCheckboxes,applyFilters} from "../modules/functions.js"
+import { crearTarjetas, getUniqueCategories, createCheckboxes, applyFilters, upcomingPastEvents } from "../modules/functions.js"
 
 let url = "https://aulamindhub.github.io/amazing-api/events.json"
-let upcomingPastEvents = [];
 let searchInput = document.getElementById("search-input");
 
-fetch(url).then(res => res.json()).then(data => {
-    let currentDate = (data.currentDate);
-    upcomingPastEvents = data.events.filter(event => (event.date) > currentDate)
-    console.log(currentDate);
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let currentDate = (data.currentDate);
+        upcomingPastEvents.push(...data.events.filter(event => event.date > currentDate));
+        console.log(currentDate);
 
-    let categories = getUniqueCategories(upcomingPastEvents);
-    createCheckboxes(categories);
-    crearTarjetas(upcomingPastEvents);
+        let categories = getUniqueCategories(upcomingPastEvents);
+        createCheckboxes(categories);
+        crearTarjetas(upcomingPastEvents);
 
-    searchInput.addEventListener("input", applyFilters);
-    document.querySelectorAll('.form-check-input').forEach(checkbox => {
-        checkbox.addEventListener("change", applyFilters);
-    }); 
-});
-
+        searchInput.addEventListener("input", () => applyFilters(upcomingPastEvents, searchInput));
+        document.querySelectorAll('.form-check-input').forEach(checkbox => {
+            checkbox.addEventListener("change", () => applyFilters(upcomingPastEvents, searchInput));
+        });
+    });
 
